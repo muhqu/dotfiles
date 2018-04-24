9661c6fad81aa0b51f290d613029c780d26c6692
Mathias Leppich <leppich@adobe.com>
2018-04-24 07:32:56 +0000

    additional key bindings for toggling apps and nice help screen

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,54 @@
 
-function initialize()
 
+hs.loadSpoon("ReloadConfiguration"):start()
+
+log = hs.logger.new('init','debug')
+
+-- CapsLock key is mapped to cmd + ctrl + shift + alt via Karabiner.
+-- https://pqrs.org/osx/karabiner/index.html
+capsLock = {'cmd','ctrl','shift','alt'}
+
+helpShown = false
+function showHelp() 
+	hs.alert.closeAll()
+	helpShown = true
+	hs.alert([[
+Global Key Bindings
+
+⇪ h			Toggle Help
+⇪ u			Type current Browser URL
+⇪ …			Toggle Apps:
+				  i - iTerm2, c - VS Code, g - GitX, 
+				  m - Mail, o - Outlook, n - Notes,
+				  b - Browser (Google Chrome)
+
+⌘ ä			Toggle Window Manage Mode
+
+In Window Manage Mode:
+
+←/→/↑/↓		Move window
+c				Cycle center
+space			Toggle fullscreen
+⌘ ←/→		Cycle ¼ ½ ¾ left/right
+⌘ ↑/↓		Cycle upper/lower left/right
+⇧ ⌘ ←/→		Move to screen
+esc			Quit mode]], 999999)
+end
+
+function hideHelp() 
+	if helpShown then
+		hs.alert.closeAll()
+		helpShown = false
+	end
+end
+
+function initialize()
+	
 	k = hs.hotkey.modal.new("cmd", "ä")
 	function k:entered()
 		hs.alert.closeAll()
-		hs.alert'Manage Windows'
+		hideHelp()
+		hs.alert("Manage Windows (h - help)", 999999)
 	end
 	function k:exited()
 		hs.alert.closeAll()
@@ -15,6 +59,14 @@ function initialize()
 	end)
 	k:bind('', 'escape', function()
 		k:exit()
+	end)
+	k:bind('', 'h', function()
+		if not helpShown then
+			showHelp()
+		else
+			hideHelp()
+			hs.alert("Manage Windows (h - help)", 999999)
+		end
 	end)
 
 	-- Space toggles the focussed between full screen and its initial size and position.
@@ -60,7 +112,6 @@ function initialize()
 	  hs.window.focusedWindow():moveOneScreenEast()
 	end)
 
-
 	k:bind('', 'UP',    pnr(function() chWinFrame(  0,-10, 0, 0) end))
 	k:bind('', 'DOWN',  pnr(function() chWinFrame(  0, 10, 0, 0) end))
 	k:bind('', 'LEFT',  pnr(function() chWinFrame(-10,  0, 0, 0) end))
@@ -75,6 +126,24 @@ function initialize()
 	k:bind('ctrl',  'DOWN',  pnr(function() chWinFrame(  0,  0,  0,-10) end))
 	k:bind('ctrl',  'LEFT',  pnr(function() chWinFrame( 10,  0,-10,  0) end))
 	k:bind('ctrl',  'RIGHT', pnr(function() chWinFrame(  0,  0,-10,  0) end))
+
+	-- Other Key Bindings
+	h = hs.hotkey.modal.new(capsLock, 'h')
+	function h:entered() showHelp() end
+	function h:exited() hideHelp() end
+	h:bind(capsLock, 'h', function() h:exit() end)
+	h:bind('', 'escape', function() h:exit() end)
+
+	hs.hotkey.bind(capsLock, 'u', function() typeCurrentBrowserURL() end)
+		
+	hs.hotkey.bind(capsLock, 'i', function() toggleApp("iTerm2") end)
+	hs.hotkey.bind(capsLock, 'c', function() toggleApp("Code") end)
+	hs.hotkey.bind(capsLock, 'b', function() toggleApp("Google Chrome") end)
+	hs.hotkey.bind(capsLock, 'g', function() toggleApp("GitX") end)
+	hs.hotkey.bind(capsLock, 'm', function() toggleApp("Mail") end)
+	hs.hotkey.bind(capsLock, 'o', function() toggleApp("Microsoft Outlook") end)
+	hs.hotkey.bind(capsLock, 'n', function() toggleApp("Notes") end)
+
 end
 
 -- Make the alerts look nicer.
@@ -133,8 +202,57 @@ end
 
 initialize()
 
-hs.loadSpoon("ReloadConfiguration"):start()
 hs.loadSpoon('RoundedCorners'):start()
 
-hs.alert'Hammerspoon loaded…'
+
+
+-- Rather than switch to Chrome, copy the current URL, switch back to the previous app and paste,
+-- This is a function that fetches the current URL from Chrome and types it
+function typeCurrentBrowserURL()
+	script = [[
+	tell application "Google Chrome" to return URL of active tab of first window
+	]]
+	ok, result = hs.applescript(script)
+	if (ok) then
+		hs.eventtap.keyStrokes(result)
+	end
+end
+
+
+-- Callback function for application events
+function applicationWatcher(appName, eventType, app)
+	if (eventType == hs.application.watcher.activated) then
+		if (appName == "Finder") then
+			-- Bring all Finder windows forward when one gets activated
+			app:selectMenuItem({"Window", "Bring All to Front"})
+		end
+	end
+end
+hs.application.watcher.new(applicationWatcher):start()
+
+
+-- toggleApp() uses application:activate to switch between an app and the
+-- previously active app. It only brings the main window (or last active window)
+-- to foreground, instead of the system task switcher (cmd+tab), which brings
+-- all application windows to foreground.
+previousApp = hs.application.frontmostApplication()
+function toggleApp(appName)
+	local app = hs.appfinder.appFromName(appName)
+	local currentApp = hs.application.frontmostApplication();
+	if not app then
+		hs.application.launchOrFocus(appName)
+	elseif not app:isFrontmost() then
+		app:activate();
+	else
+		previousApp:activate();
+	end
+	previousApp = currentApp;
+end
+
+
+function notify(message)
+	hs.notify.new({title="Hammerspoon", informativeText=message}):send()
+end
+
+notify'configuration loaded! 🎉'
 
